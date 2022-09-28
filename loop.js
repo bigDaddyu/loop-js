f285fa89c0639a55e0957108b32dc378d651cf4c
bigDaddyu <abusideeq123@gmail.com>
2022-09-28 13:26:55 -0700

    created a new js file

---
+++ loop.js
@@ -0,0 +1,19 @@
+console.log(`Question 0`)
+for (let i=1; i<=10; i++){
+    console.log(i);
+}
+
+console.log(`Odd numbers`)
+for (let j=1; j<=100; j+=2){
+     console.log(j);
+}
+
+console.log(`Multiples of 5`)
+for (let k=5; k<=100; k+=5){
+    console.log(k);
+}
+
+console.log(`Prime numbers`)
+for(let k=1; k<=100; k+=2){
+    console.log(k);
+}
